e7c82d969fb9ec1dbe851b5d27620e043eaf8369
zhikai1993 <czhikai1993@gmail.com>
2019-06-27 20:42:16 -0700

    fix importing db, add in run.py

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 jwt = JWT(app, authenticate, identity)
 
