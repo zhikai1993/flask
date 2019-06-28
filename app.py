6f4ee0aeca02dc4e050fd7a490e71b9d72da8a6a
zhikai1993 <czhikai1993@gmail.com>
2019-06-27 20:54:30 -0700

    added pstergre env

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +10,9 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#first arg is env arg, second is default
+app.config['SQLAlCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
 app.secret_key = 'jose'
